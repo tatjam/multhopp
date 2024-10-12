13256e206a75a834e5889d661e52bd48b91130a5
tatjam <tatjam02@gmail.com>
2024-10-12 16:38:17 +0200

    Nearly done

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+#[macro_use]
 extern crate approx as approx;
+
 extern crate faer as fa;
 use core::{f64, num};
 
-use approx::*;
 use fa::{prelude::SpSolver, FaerMat};
-use plotters::prelude::*;
+use plotters::prelude::{DiscreteRanged, IntoLinspace};
+use std::{fs::File, io::Write};
 
 mod plot;
 
@@ -22,7 +24,8 @@ fn cosine_samples(num_points: usize, b: f64) -> (Vec<f64>, Vec<f64>) {
     return (thetas, ys);
 }
 
-// Returns A coefficients
+// Returns A coefficients, given functions take non dimensional
+// spanwise position that ranges from -1 to 1
 fn multhopp(
     cuerda: fn(f64) -> f64,
     alpha: fn(f64) -> f64,
@@ -31,10 +34,11 @@ fn multhopp(
     b: f64,
 ) -> fa::Mat<f64> {
     let (thetas, ys) = cosine_samples(num_points, b);
+    let ys_adim: Vec<f64> = ys.iter().map(|y| 2.0 * y / b).collect();
 
-    let c_samples: Vec<f64> = ys.iter().map(|y| cuerda(*y)).collect();
-    let alpha_samples: Vec<f64> = ys.iter().map(|y| alpha(*y)).collect();
-    let clalpha_samples: Vec<f64> = ys.iter().map(|y| clalpha(*y)).collect();
+    let c_samples: Vec<f64> = ys_adim.iter().map(|y| cuerda(*y)).collect();
+    let alpha_samples: Vec<f64> = ys_adim.iter().map(|y| alpha(*y)).collect();
+    let clalpha_samples: Vec<f64> = ys_adim.iter().map(|y| clalpha(*y)).collect();
 
     let mut mat = fa::Mat::<f64>::zeros(num_points, num_points);
     let mut rhs = fa::Mat::<f64>::zeros(num_points, 1);
@@ -57,66 +61,141 @@ fn multhopp(
         rhs[(j, 0)] = alpha;
     }
 
-    println!("{:?}", mat);
-    println!("{:?}", rhs);
-
     let sln = mat.full_piv_lu().solve(rhs);
 
     return sln;
 }
 
-const B: f64 = 12.0;
+// We exploit the fact that AR = b^2 / S
+// S is int_(-b/2)^(b/2) c(y) dy
+// by changing variable to y' = 2 y / b we find that
+// b^2 / S = b^2 / b int_(-1)^(1) c(y') dy' = 2 b / int_(-0.5)^(0.5) c(y') dy'
+// We use rectangle integration with heaps of points to get accurate
+fn estimate_AR(cuerda: fn(f64) -> f64, b: f64) -> f64 {
+    let dyp = 0.001;
+    let mut int = 0.0;
+    let mut yp = -1.0;
+    while yp <= 1.0 {
+        int += cuerda(yp) * dyp;
+        yp += dyp;
+    }
+    return 2.0 * b / int;
+}
 
-fn c_rectangular(y: f64) -> f64 {
-    10.0
+fn run_case(
+    name: &str,
+    cuerda: fn(f64) -> f64,
+    alpha: fn(f64) -> f64,
+    clalpha: fn(f64) -> f64,
+    num_points: usize,
+    b: f64,
+) {
+    let fname = [name, ".txt"].concat();
+    let mut ofile = File::options()
+        .write(true)
+        .create(true)
+        .open(fname)
+        .unwrap();
+
+    let sln = multhopp(cuerda, alpha, clalpha, num_points, b);
+
+    writeln!(&mut ofile, "{:?}", sln).unwrap();
+    let k = {
+        let mut sum = 0.0;
+        let base = sln[(0, 0)] * sln[(0, 0)];
+        // i = 1 means n = 2!
+        for i in 1..sln.nrows() {
+            let n = (i + 1) as f64;
+            sum += n * sln[(i, 0)] * sln[(i, 0)] / base;
+        }
+        1.0 + sum
+    };
+    writeln!(&mut ofile, "ostwald k = {}", k).unwrap();
+
+    let plotname = [name, ".png"].concat();
+    let mut ctx = plot::make_default_plot(&plotname, b);
+    plot::add_cl(&mut ctx, &sln);
+    plot::add_geom("cuerda", &mut ctx, cuerda, 0.05);
+    plot::finish(ctx);
 }
 
 fn main() {
-    let root_area = BitMapBackend::new("out/test.png", (1024, 768)).into_drawing_area();
-    root_area.fill(&WHITE).unwrap();
+    run_case(
+        "out/rectangular",
+        |_| 1.0,
+        |_| 0.1,
+        |_| 2.0 * std::f64::consts::PI,
+        30,
+        1.0,
+    );
+    run_case(
+        "out/elliptical",
+        |y| {
+            // y goes from -1 to 1
+            (1.0 - y * y).sqrt()
+        },
+        |_| 0.1,
+        |_| 2.0 * std::f64::consts::PI,
+        30,
+        1.0,
+    );
+}
 
-    let sln = multhopp(c_rectangular, |_| 0.15, |_| 5.1, 15, B);
-    let b_range = (-B / 2.0)..(B / 2.0);
+#[cfg(test)]
+mod test {
+    use super::*;
 
-    println!("{:?}", sln);
+    #[test]
+    fn ar_rectangles() {
+        // In rectangles, AR is simply b / c
+        assert_relative_eq!(estimate_AR(|_| 1.0, 1.0), 1.0, max_relative = 0.001);
+        assert_relative_eq!(estimate_AR(|_| 2.0, 1.0), 0.5, max_relative = 0.001);
+        assert_relative_eq!(estimate_AR(|_| 1.0, 2.0), 2.0, max_relative = 0.001);
+    }
 
-    let mut cc = ChartBuilder::on(&root_area)
-        .margin(5)
-        .set_all_label_area_size(50)
-        .build_cartesian_2d(b_range.clone(), -0.2f32..0.2)
-        .unwrap();
+    #[test]
+    fn ar_trapezoid() {
+        // Mean chord is that such that b * SMC = S
+        // Thus in a trapezoid, with S = b * c_end + 0.5 * b * (c_start - c_end)
+        // we have that SMC = c_end + 0.5 * (c_start - c_end)
+        // (AR = b / SMC)
+        assert_relative_eq!(
+            estimate_AR(|yp| 1.0 - yp.abs() * 0.5, 1.0),
+            1.0 / (0.5 + 0.5 * (1.0 - 0.5)),
+            max_relative = 0.001
+        );
+        assert_relative_eq!(
+            estimate_AR(|yp| 2.0 - yp.abs(), 1.0),
+            1.0 / (1.0 + 0.5 * (2.0 - 1.0)),
+            max_relative = 0.001
+        );
+        assert_relative_eq!(
+            estimate_AR(|yp| 3.0 - yp.abs() * 0.5, 2.0),
+            2.0 / (2.5 + 0.5 * (3.0 - 2.5)),
+            max_relative = 0.001
+        );
+    }
 
-    cc.configure_mesh()
-        .x_labels(20)
-        .y_labels(15)
-        .draw()
-        .unwrap();
-
-    let b_range_step = b_range.step(0.001);
-
-    cc.draw_series(LineSeries::new(
-        b_range_step.values().map(|y| {
-            let theta = (2.0 * y / B).acos();
-            let mut sum = 0.0;
-            for i in 0..sln.nrows() {
-                let n = i + 1;
-                sum += sln[(i, 0)] * ((n as f64) * theta).sin();
-            }
-            (y, sum as f32)
-        }),
-        &RED,
-    ))
-    .unwrap()
-    .label("cl");
-
-    cc.draw_series(LineSeries::new(
-        b_range_step
-            .values()
-            .map(|y| (y, c_rectangular(y) as f32 * 0.001)),
-        &BLUE,
-    ))
-    .unwrap()
-    .label("cl");
-
-    root_area.present().unwrap();
+    #[test]
+    fn ar_ellipses() {
+        // The surface area of a semi-ellipse of semiaxes A, B is 0.5 * pi * A * B, thus
+        // SMC = 0.5 * pi * A * B / b. Now, B = b / 2, thus
+        // SMC = 0.25 * pi * A
+        // Hence for an ellipse AR = b / (0.25 * pi * A)
+        assert_relative_eq!(
+            estimate_AR(|yp| (1.0 - yp * yp).sqrt(), 1.0),
+            1.0 / (0.25 * std::f64::consts::PI * 1.0),
+            max_relative = 0.001
+        );
+        assert_relative_eq!(
+            estimate_AR(|yp| (1.0 - yp * yp).sqrt() * 3.0, 1.0),
+            1.0 / (0.25 * std::f64::consts::PI * 3.0),
+            max_relative = 0.001
+        );
+        assert_relative_eq!(
+            estimate_AR(|yp| (1.0 - yp * yp).sqrt(), 2.0),
+            2.0 / (0.25 * std::f64::consts::PI * 1.0),
+            max_relative = 0.001
+        );
+    }
 }
---
+++ src/plot.rs
@@ -0,0 +1,90 @@
+use plotters::coord::*;
+use plotters::prelude::*;
+use types::RangedCoordf32;
+use types::RangedCoordf64;
+
+pub struct PlotContext<'a> {
+    cc: ChartContext<'a, BitMapBackend<'a>, Cartesian2d<RangedCoordf64, RangedCoordf32>>,
+    root_area: DrawingArea<BitMapBackend<'a>, Shift>,
+    b_range: std::ops::Range<f64>,
+}
+
+pub fn make_default_plot<'a>(name: &'a str, b: f64) -> PlotContext<'a> {
+    let b_range = (-b / 2.0)..(b / 2.0);
+
+    let root_area = BitMapBackend::new(name, (1024, 768)).into_drawing_area();
+    root_area.fill(&WHITE).unwrap();
+
+    let mut cc = ChartBuilder::on(&root_area)
+        .margin(5)
+        .set_all_label_area_size(50)
+        .build_cartesian_2d(b_range.clone(), -0.2f32..0.2)
+        .unwrap();
+
+    cc.configure_mesh()
+        .x_labels(20)
+        .y_labels(15)
+        .draw()
+        .unwrap();
+
+    return PlotContext {
+        cc: cc,
+        root_area: root_area,
+        b_range: b_range,
+    };
+}
+
+pub fn add_cl(ctx: &mut PlotContext, sln: &fa::Mat<f64>) {
+    let b_range_step = ctx.b_range.clone().step(0.001);
+    let b = ctx.b_range.end - ctx.b_range.start;
+    ctx.cc
+        .draw_series(LineSeries::new(
+            b_range_step.values().map(|y| {
+                let theta = (2.0 * y / b).acos();
+                let mut sum = 0.0;
+                for i in 0..sln.nrows() {
+                    let n = i + 1;
+                    sum += sln[(i, 0)] * ((n as f64) * theta).sin();
+                }
+                // We assume incoming air speed of 1m/s
+                sum *= 1.225 * b;
+                (y, sum as f32)
+            }),
+            &RED,
+        ))
+        .unwrap()
+        .label("cl");
+}
+
+pub fn add_geom(name: &str, ctx: &mut PlotContext, cuerda: fn(f64) -> f64, scale: f64) {
+    let b_range_step = ctx.b_range.clone().step(0.001);
+    let b = ctx.b_range.end - ctx.b_range.start;
+    // We place the top line such that it's 1/4 of the chord
+    ctx.cc
+        .draw_series(LineSeries::new(
+            b_range_step.values().map(|y| {
+                let adimy = 2.0 * y / b;
+                (y, (cuerda(adimy) * scale * 0.25) as f32)
+            }),
+            &BLUE,
+        ))
+        .unwrap()
+        .label(name);
+
+    // And the bottom line such that it's the remainder
+    ctx.cc
+        .draw_series(LineSeries::new(
+            b_range_step.values().map(|y| {
+                let adimy = 2.0 * y / b;
+                (y, (-cuerda(adimy) * scale * 0.75) as f32)
+            }),
+            &BLUE,
+        ))
+        .unwrap()
+        .label(name);
+}
+
+// This "consumes" the plot context
+pub fn finish(ctx: PlotContext) {
+    ctx.root_area.present().unwrap();
+}
